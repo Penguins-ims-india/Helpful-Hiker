de8b98b00af0149eee5866eaaecc0cc4f78d7e83
jrob112 <josh.roberts0527@gmail.com>
2024-05-21 18:40:29 -0500

    added weather route file and started working on the routes

---
+++ server/routes/weather.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const axios = require('axios');
+const router = Router();
+require('dotenv').config();
+
+router.get('/weather', (req, res) => {
+  
+});
+
+router.post('/weather', (req, res) => {
+
+});
+
+module.exports = router;
