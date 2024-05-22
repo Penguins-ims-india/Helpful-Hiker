95877f5eba757e6044414df4a55085d0aff73778
uhrinuh <nasri.arina@gmail.com>
2024-05-22 14:29:28 -0700

    finished packinglist routes

--- server/routes/packingList.js
+++ server/routes/packingList.js
@@ -2,56 +2,6 @@ const express = require('express');
 const packingList = express.Router();
 const { PackingList } = require('../database/index.js');
 
-/*
-NOTE
-TO TEST THIS:
-in client/components/App.jsx added this to App
-  const handleClick = () => {
-    axios.get('/packingList')
-    .then((response) => {
-      console.log('response data', response.data);
-    })
-    .catch((err) => {
-      console.error('could not get user data', err)
-    });
-  }
-and in the return statement i added this
-  <button onClick={handleClick}>get packing list</button>
-navigated to localhost:3000 then signed in, then clicked get packing list
-added this to packingList.get
-  console.log('Request received:', req.headers);
-    Request received: {
-    host: 'localhost:3000',
-    connection: 'keep-alive',
-    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-    accept: 'application/json, text/plain, *',
-    'sec-ch-ua-mobile': '?0',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    referer: 'http://localhost:3000/home',
-    'accept-encoding': 'gzip, deflate, br, zstd',
-    'accept-language': 'en-US,en;q=0.9',
-    cookie: 'connect.sid=s%3A7_Uu_LcQvlPPlxTnKotAkw1awTvPLUdz.ceCdv2OEqPCq88una%2FD1x1C9l85XJ5O99CO7oA%2B7FcQ'
-  }
-  console.log('Request user:', req.user)
-    Request user: [
-  {
-    id: 1,
-    username: null,
-    googleId: '115005075666417817657',
-    createdAt: '2024-05-21T23:36:31.000Z',
-    updatedAt: '2024-05-21T23:36:31.000Z'
-  },
-  false
-]
-  const userId = req.user[0]['id'];
-  console.log('user id', userId);
-  user id 1
-*/
-
 // get packing list for a specific user
 packingList.get('/packingList', (req, res) => {
   // PackingList from server/database/index.js references the User id so lets use that
@@ -71,13 +21,17 @@ packingList.get('/packingList', (req, res) => {
 
 // create a new packing list
 packingList.post('/packingList', (req, res) => {
+  // get access to the user's id
   const userId = req.user[0]['id'];
+  // destructure name from req.body
   const { name } = req.body;
-  console.log('name: ', name);
+  // use create sequelize prop to make a new packinglist
   PackingList.create({ userId, name })
+  // set status to 201 and send new packinglist
     .then((newPackingList) => {
       res.status(201).json(newPackingList);
     })
+    // if the query fails, send 500 sc and error message
     .catch((err) => {
       console.error('Error creating packing list', err);
       res.sendStatus(500);
@@ -85,18 +39,75 @@ packingList.post('/packingList', (req, res) => {
 });
 
 // get a specific packing list by its id
-packingList.get('packingList/:id', (req, res) => {
+packingList.get('/packingList/:id', (req, res) => {
+  // get access to the user's id and destructure id from req.params
+  const userId = req.user[0]['id'];
+  const { id } = req.params;
 
+  // use findOne sequelize method to find the specific list
+  PackingList.findOne({ where: { userId, id } })
+  // if you find the list, send it but if you don't send a 404 sc
+  .then((packingList) => {
+    if (packingList) {
+      res.json(packingList);
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  // catch block if the packing list isn't found
+  .catch((err) => {
+    console.error('Error getting specific packing list', err);
+    res.sendStatus(500);
+  })
 });
 
 // update an existing packing list by its id
-packingList.put('packingList/:id', (req, res) => {
+packingList.put('/packingList/:id', (req, res) => {
+  // get access to the user's id and destructure id from params and name from req.body
+  const userId = req.user[0]['id'];
+  const { id } = req.params;
+  const { name } = req.body;
 
+  // use update sequelize method to update the name of the packing list
+  PackingList.update({ name }, { where: { userId, id } })
+    .then(([affectedRows]) => {
+      // if there is an affected row, sc 200
+      if (affectedRows > 0) {
+        res.sendStatus(200);
+        // if there isn't, sc 404
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    // catch block if unable to update packing list
+    .catch((err) => {
+      console.error('Error updating packing list', err);
+      res.sendStatus(500);
+    });
 });
 
 // delete a specific packing list by its id
-packingList.delete('packingList/:id', (req, res) => {
+packingList.delete('/packingList/:id', (req, res) => {
+  // get access to the user's id and destructure id from req.params
+  const userId = req.user[0]['id'];
+  const { id } = req.params;
 
+  // use destroy sequelize method to delete a specific packing list
+  PackingList.destroy({ where: { userId, id } })
+    .then((affectedRows) => {
+      // if there is an affected row, sc 200
+      if (affectedRows > 0) {
+        res.sendStatus(200);
+        // if there isn't, sc 404
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    // catch block if unable to update packing list
+    .catch((err) => {
+      console.error('Error deleting packing list', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = packingList;
