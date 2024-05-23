3df171dde0b72fb2af245852848de9430b95d85f
Hackman78 <pathenry231@gmail.com>
2024-05-23 17:21:08 -0500

    added Map.jsx

---
+++ client/components/Map.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '1000px',
+  height: '1000px'
+};
+
+const center = {
+  lat: 29.9511,
+  lng: -90.0715
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyAHStHEGequcbfK849wQ4v3IUgyFpNuEIM'
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback((map) => {
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      { /* Child components, such as markers, info windows, etc. */ }
+      <></>
+    </GoogleMap>
+  ) : <></>;
+}
+
+export default React.memo(Map);
