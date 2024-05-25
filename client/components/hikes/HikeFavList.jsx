6dad6dd165fdc8d1829c360e9ffc5f091c4022c1
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-24 21:40:17 -0500

    ADD search bar to search by tag and clear filter button to show all hikes

--- client/components/hikes/HikeFavList.jsx
+++ client/components/hikes/HikeFavList.jsx
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import HikeFav from './HikeFav.jsx';
+import { TextField, Button } from '@mui/material';
 
 const HikeFavList = ({ favHikes, getFavHikes, allTags }) => {
 
-  const [filter, setFilter] = useState('all')
+  const [filter, setFilter] = useState('all');
+  const [input, setInput] = useState('');
 
   const changeFilter = (newFilter) => {
     if (favHikes.hasOwnProperty(newFilter)) { setFilter(newFilter); }
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
   }
 
   const filteredHikes = favHikes[filter] || [];
 
   return (
     <div className="fav-hike-list">
+        <TextField
+          value={input}
+          placeholder='Search by tag'
+          onChange={handleChange}
+        />
+        <Button onClick={() => {changeFilter(input)}}>Search</Button>
+        {filter === 'all' ? <></> : <Button onClick={() => {changeFilter('all')}}>Clear Filter</Button>}
       {
         filteredHikes.map((favHike, i) => {
           return (
