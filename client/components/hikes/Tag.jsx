e39b334506190f91dff57016e0e3b0acfe0eebc7
AlexPHebert2000 <aphebert9994@gmail.com>
2024-05-26 19:42:02 -0500

    ADD click tag to filter

--- client/components/hikes/Tag.jsx
+++ client/components/hikes/Tag.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Chip } from "@mui/material";
 
-const Tag = ({ tag, deleteTag, handleClick }) => {
+const Tag = ({ tag, deleteTag, handleClick, changeFilter }) => {
   const { name, color } = tag;
   const styling = {backgroundColor: 'white', color, margin: 1, borderColor: color}
   return (
     deleteTag ?
-    <Chip variant="outlined" size="small" sx={styling} label={name} onDelete={() => {deleteTag(tag.id)}}/> :
+    <Chip variant="outlined" size="small" sx={styling} label={name} onClick={() => changeFilter(name)} onDelete={() => {deleteTag(tag.id)}}/> :
     <Chip variant='outlined' size="small" sx={styling} label={name} onClick={() => handleClick(tag)}/>
   )
 }
