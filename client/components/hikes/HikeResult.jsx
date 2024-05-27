913f809f4e93f2c08192829b4fe5344e27f120b9
jrob112 <josh.roberts0527@gmail.com>
2024-05-27 08:49:54 -0500

    merge commit

--- client/components/hikes/HikeResult.jsx
+++ client/components/hikes/HikeResult.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import { Button, List, ListItem, ListItemText } from '@mui/material';
 import Weather from '../Weather.jsx';
+import { Button, TextField, Card, Typography, Box, IconButton, Menu, MenuItem, Divider, Rating} from '@mui/material';
+import styles from '../style'
+
 
 const HikeResult = ({ hike, getFavHikes }) => {
 
@@ -32,7 +35,8 @@ const HikeResult = ({ hike, getFavHikes }) => {
   }
   
   return (
-    <div>
+    <Card variant='outlined' sx={{width: 1/4, borderColor: 'black', margin: 3}}>
+    <Box >
       <List>
         <ListItem>
           <Weather location={location}/>
@@ -40,21 +44,22 @@ const HikeResult = ({ hike, getFavHikes }) => {
         <ListItem>
           <ListItemText
             primary={`Name:  ${description}`}
-          />
+            />
         </ListItem>
         <ListItem>
           <ListItemText
             primary={`Location:  ${location}`}
-          />
+            />
         </ListItem>
         <ListItem>
           <ListItemText
             primary={`Rating:  ${rating}`}
-          />
+            />
         </ListItem>
       </List>
       <Button variant="outlined" onClick={ addFavHike } type="button">Add Favorite</Button>
-    </div>
+    </Box>
+    </Card >
   )
 }
 
